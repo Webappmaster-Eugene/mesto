567828de12b28c5012db0c8e933329952cf74395
Numart <johnn.hotmail@mail.ru>
2022-11-10 19:36:29 +0300

    fix: version 1.2 after the first review

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,29 @@
-let profileChanger = document.querySelector('.profile__changer');
-let button = document.querySelector('.button');
+let profileOpen = document.querySelector('.profile__changer');
 let popupClose = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
-
+let popupForm = document.querySelector('.popup');
+let popupButton = document.querySelector('.popup__button');
 let inputName = document.querySelector('.popup__input_type_name');
 let inputInfo = document.querySelector('.popup__input_type_info');
-
-profileChanger.addEventListener('click', function(){
-    popup.classList.toggle('popup_display-none');
-});
-
-popupClose.addEventListener('click', function(){
-    popup.classList.toggle('popup_display-none');
-});
-
-button.addEventListener('click', function(){
-    popup.classList.toggle('popup_display-none');
-    profileName.textContent = inputName.value;
-    profileStatus.textContent = inputInfo.value;
-});
 
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 
-inputName.value = profileName.textContent;
-inputInfo.value = profileStatus.textContent;
-
 let placeLikes = document.querySelectorAll('.place__like');
+
+profileOpen.addEventListener('click', function(){
+    popupForm.classList.toggle('popup_opened');
+});
+
+popupClose.addEventListener('click', function(){
+    popupForm.classList.toggle('popup_opened');
+});
+
+popupButton.addEventListener('click', function(){
+    profileName.textContent = inputName.value;
+    profileStatus.textContent = inputInfo.value;
+    console.log('dfdfdf');
+    popupForm.classList.toggle('popup_opened');
+});
 
 for (let placeLike of placeLikes) {
     placeLike.addEventListener('click', function(){
@@ -34,3 +31,5 @@ for (let placeLike of placeLikes) {
     });
 }
 
+inputName.value = profileName.textContent;
+inputInfo.value = profileStatus.textContent;
